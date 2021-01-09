caf3a9ce518cead7281669133413339d93b8b0e1
Anas Alsmele <52325883+AnasMusic@users.noreply.github.com>
2021-01-10 01:11:29 +0300

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -1,7 +1,7 @@
 var lang = {
     "errors":{
         "e":"Error: ",
-        "ue":"an error has been occurred, please try to refresh the page and try again",
+        "ue":"an error has been occurred, please refresh the page and try again",
         "inf":"item not found",
         "tnf":"tag not found"
     }
